2abdb72e04b96adb004301a2b923158784e8c949
barros0 <joaoaobarros7@gmail.com>
2022-10-27 16:22:34 +0100

    update

---
+++ EN3/2211695AndreSousa/Landing Page/js/scripts.js
@@ -0,0 +1,39 @@
+//menu hamburguer
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+//header mudar cor
+$(function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $(".navbarr").addClass("active");
+      } else {
+          //remove the background property so it comes transparent again (defined in your css)
+         $(".navbarr").removeClass("active");
+      }
+  });
+});
+
+//contador
+var countDownDate = new Date("Oct 31, 2022 15:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("contador").innerHTML = days + " Dias " + hours + " Horas "
+  + minutes + " Minutos " + seconds + " Segundos ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("contador").innerHTML = "EXPIRADO";
+  }
+}, 1000);
