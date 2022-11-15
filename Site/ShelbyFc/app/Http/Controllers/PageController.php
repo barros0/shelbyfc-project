c094b9639f5395cc99baae7b194be51aed826dc8
submi <submica99@gmail.com>
2022-11-15 16:17:29 +0000

    4

--- Site/ShelbyFc/app/Http/Controllers/PageController.php
+++ Site/ShelbyFc/app/Http/Controllers/PageController.php
@@ -10,4 +10,14 @@ class PageController extends Controller
 
         return view('index');
     }
+
+
+    public function dologin(Request $request){
+
+        if (Auth::attempt(['email'=>$request->email], ['password'=>$request->password])) {
+            return redirect()->route('index');
+        }
+
+        return back();
+    }
 }
