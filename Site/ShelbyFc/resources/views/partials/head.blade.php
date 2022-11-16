e1c7a13c66bc1ef527dbf61e04263ffa94dd8dec
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2022-11-16 22:36:55 +0000

    sd

--- Site/ShelbyFc/resources/views/partials/head.blade.php
+++ Site/ShelbyFc/resources/views/partials/head.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
+    <link rel="icon" type="{{asset('images/logo.ico')}}">
+
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/styles.css')}}">
