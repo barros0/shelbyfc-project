67803fc3416dd1f669a783650ab41289177d8b07
submi <submica99@gmail.com>
2022-11-30 15:54:40 +0000

    o k

--- Site/ShelbyFc/resources/views/auth/register.blade.php
+++ Site/ShelbyFc/resources/views/auth/register.blade.php
@@ -1,77 +1,115 @@
-@extends('layouts.app')
+@include('partials.head')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<header>
+    <div class="navbar-esq">
+        <div class="logo">
+            <a class="link-logo" href="{{route('index')}}">
+                <img src="{{asset('images/logo.svg')}}" alt="Logo">
+            </a>
         </div>
     </div>
+
+    <div class="navbar-dir">
+
+        <a href="{{route('login')}}" id="conta">INICIAR SESSÃO</a>
+
+    </div>
+</header>
+
+
+<div id="seccao-registo" class="d-flex justify-content-center align-items-center fullvh"
+     style="">
+
+    <section id="registo" class="col-12">
+
+        <div class="row text-center col-12 title-login">
+            <h3 class="tit">Torne-se Membro!</h3>
+        </div>
+
+        <div class="login-wrapper">
+            <div class="login-regular">
+                <form action="login.html">
+
+                    <div class="tab">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="name" id="nome" placeholder="Nome" required>
+                        <label for="nome">Sobrenome</label>
+                        <input type="text" name="sobrenomesubname" id="sobrenome" placeholder="Sobrenome" required>
+                    </div>
+
+                    <div class="tab">
+                        <label for="email_registo">EMAIL</label>
+                        <input type="email" id="email_registo" name="email" placeholder="nome@exemplo.com" required>
+                        <label for="telefone">Telefone</label>
+                        <input type="tel" name="phone" id="telefone" placeholder="Telefone">
+                    </div>
+
+                    <div class="tab">
+                        <label for="password_registo">PASSWORD</label>
+                        <div class="pass-container">
+                            <input type="password" name="password" id="password_registo"
+                                   placeholder="Password" required>
+                            <i class="fas fa-eye" id="eye_registo" onclick="showPassRegisto()"></i>
+                        </div>
+                        <label for="password_confirm">REPETIR PASSWORD</label>
+                        <div class="pass-container">
+                            <input type="password" name="password_confirm" id="password_confirm"
+                                   placeholder="Confirmar password" required>
+                            <i class="fas fa-eye" id="eye_confirm" onclick="showPassConfirm()"></i>
+                        </div>
+
+                    </div>
+
+
+                    <div class="botoes">
+                        <button type="button" id="prevBtn" class="btn-registo"
+                                onclick="nextPrev(-1)">VOLTAR
+                        </button>
+                        <button type="button" id="nextBtn" class="btn-registo"
+                                onclick="nextPrev(1)">SEGUINTE
+                        </button>
+
+                    </div>
+                    <div style="text-align:center;margin-top:40px;">
+                        <span class="step"></span>
+                        <span class="step"></span>
+                        <span class="step"></span>
+                    </div>
+                </form>
+            </div>
+            <div class="vl">
+                <span class="vl-innertext">OU</span>
+            </div>
+            <div class="login-socials">
+
+                <a href="{{ route('social.redirect','google') }}" class="social">
+                    <div class="logo-social"><img src="{{asset('images/icons/google.svg')}}" alt="Google"></div>
+                    <div class="nome-social">Continuar com o Google</div>
+                </a>
+
+                <a href="{{ route('social.redirect','facebook') }}" class="social">
+                    <div class="logo-social"><img src="{{asset('images/icons/apple.svg')}}" alt="Apple"></div>
+                    <div class="nome-social">Continuar com a Apple</div>
+                </a>
+
+                <a href="{{ route('social.redirect','facebook') }}"  class="social">
+                    <div class="logo-social"><img src="{{asset('images/icons/facebook.svg')}}" alt="Facebook"></div>
+                    <div class="nome-social">Continuar com o Facebook</div>
+                </a>
+            </div>
+
+        </div>
+
+        <div class="row footer-bg col-12 title-login"></div>
+
+    </section>
+
 </div>
-@endsection
+<div class="background">
+    <div class="dir"></div>
+    <div class="left"></div>
+</div>
+
+
+<script src="{{asset('js/functions.js')}}"></script>
