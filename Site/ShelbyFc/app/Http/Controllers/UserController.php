f2fcc0d02b5a96ddcebf7d19dec9434aee0890f1
submi <submica99@gmail.com>
2022-12-06 17:40:45 +0000

    up

--- Site/ShelbyFc/app/Http/Controllers/UserController.php
+++ Site/ShelbyFc/app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Models\Game;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Auth;
+use Session;
+use Str;
 
 class UserController extends Controller
 {
@@ -15,9 +17,49 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function comprar_bilhete($gameid, Request $request){
+    public function atualizar(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'nullable',
+            'pais' => 'nullable|exists:countrys',
+            'codigo_postal' => 'nullable',
+            'nif' => 'nullable',
+        ]);
 
-        $this->validate($request,[
+        $user = Auth::user();
+
+        if($request->photo){
+            $photo = $request->photo;
+            $name_photo = 'Profile_photo-'.Auth::id().'-'.Str::random(5) . '.png';
+            Storage::put('users/' . $name_photo, $photo);
+            $user->photo = $name_photo;
+        }
+
+        $user->nome = $request->nome;
+        $user->nome = $request->nome;
+        $user->nome = $request->nome;
+        $user->nome = $request->nome;
+        $user->address = $request->morada;
+        $user->pais_id = $request->pais;
+        $user->postal_code = $request->codigo_postal;
+        $user->nif = $request->nif;
+        $user->save();
+
+        Session::flash('success', 'Os seus dados foram atualizados!');
+        return back();
+
+
+
+
+
+    }
+
+    public function comprar_bilhete($gameid, Request $request)
+    {
+
+        $this->validate($request, [
             'quantity' => 'required|numeric|min:1',
         ]);
 
@@ -32,8 +74,35 @@ class UserController extends Controller
             $ticket->price = $game->ticket_price;
             $ticket->save();
         }
-        
-        Return;
+
+        return;
+    }
+
+
+    public function changePasswordPost(Request $request) {
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password.");
+        }
+
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            // Current password and new password same
+            return redirect()->back()->with("error","New Password cannot be same as your current password.");
+        }
+
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new_password' => 'required|string|min:8|confirmed',
+            'confirm_password' => 'required|string|min:8|confirmed',
+        ]);
+
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        Session::flash('success', 'A sua password foi atualizada!');
+        return redirect()->back();
     }
 
     public function index()
@@ -54,7 +123,7 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -65,7 +134,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -76,7 +145,7 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -87,8 +156,8 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -99,7 +168,7 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
