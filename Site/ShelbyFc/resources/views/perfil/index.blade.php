4fd05751de51f82f0994f6db201716c318413f5e
submi <submica99@gmail.com>
2022-12-07 14:49:58 +0000

    e

--- Site/ShelbyFc/resources/views/perfil/index.blade.php
+++ Site/ShelbyFc/resources/views/perfil/index.blade.php
@@ -8,69 +8,73 @@
 
     <section class="col-12 ">
 
-        <div class=" vh-100 d-flex flex-wrap">
+        <div class=" vh-100 d-flex flex-wrap  justify-content-center">
 
-            <nav class="nav-conta container-user-settings ">
-                <div class="nav-conta-wrap">
-                    <div class="title">
-                        <h3>Definições de conta</h3>
+
+
+            <div class="tab-content d-flex justify-content-center">
+
+                <nav class="nav-conta container-user-settings ">
+                    <div class="nav-conta-wrap">
+                        <div class="title">
+                            <h3>Definições de conta</h3>
+                        </div>
+
+                        <ul class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist"
+                            aria-orientation="vertical">
+                            <li>
+                                <a href="#" class="active"
+                                   class="active" id="v-pills-perfil-tab" data-bs-toggle="pill"
+                                   data-bs-target="#v-pills-perfil" type="button" role="tab" aria-controls="v-pills-perfil"
+                                   aria-selected="true"
+                                >
+                                    <i class="fa fa-user"></i>
+                                    Perfil
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" id="v-subscricoes-tab" data-bs-toggle="pill"
+                                   data-bs-target="#v-subscricoes" type="button" role="tab"
+                                   aria-controls="v-subscricoes" aria-selected="false">
+                                    <i class="fa-sharp fa-solid fa-handshake"></i>
+                                    Subscrições
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" id="v-seguranca-tab" data-bs-toggle="pill"
+                                   data-bs-target="#v-seguranca" type="button" role="tab"
+                                   aria-controls="v-seguranca" aria-selected="false">
+                                    <i class="fa fa-lock"></i>
+                                    Segurança
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" id="v-transacoes-tab" data-bs-toggle="pill"
+                                   data-bs-target="#v-transacoes" type="button" role="tab"
+                                   aria-controls="v-transacoes" aria-selected="false">
+                                    <i class="fa fa-coins"></i>
+                                    Transações
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" id="v-preferencias-tab" data-bs-toggle="pill"
+                                   data-bs-target="#v-preferencias" type="button" role="tab"
+                                   aria-controls="v-preferencias" aria-selected="false">
+                                    <i class="fa-solid fa-arrow-right-arrow-left"></i>
+                                    Preferências
+                                </a>
+                            </li>
+                        </ul>
+
+                        <div class="logout d-flex justify-content-center align-items-center">
+                            <a href="{{route('logout')}}">
+                                <i class="fa-sharp fa-solid fa-arrow-right-from-bracket"></i>
+                                <p>Sair</p>
+                            </a>
+                        </div>
                     </div>
+                </nav>
 
-                    <ul class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist"
-                        aria-orientation="vertical">
-                        <li>
-                            <a href="#" class="active"
-                               class="active" id="v-pills-perfil-tab" data-bs-toggle="pill"
-                               data-bs-target="#v-pills-perfil" type="button" role="tab" aria-controls="v-pills-perfil"
-                               aria-selected="true"
-                            >
-                                <i class="fa fa-user"></i>
-                                Perfil
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" id="v-subscricoes-tab" data-bs-toggle="pill"
-                               data-bs-target="#v-subscricoes" type="button" role="tab"
-                               aria-controls="v-subscricoes" aria-selected="false">
-                                <i class="fa-sharp fa-solid fa-handshake"></i>
-                                Subscrições
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" id="v-seguranca-tab" data-bs-toggle="pill"
-                               data-bs-target="#v-seguranca" type="button" role="tab"
-                               aria-controls="v-seguranca" aria-selected="false">
-                                <i class="fa fa-lock"></i>
-                                Segurança
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" id="v-transacoes-tab" data-bs-toggle="pill"
-                               data-bs-target="#v-transacoes" type="button" role="tab"
-                               aria-controls="v-transacoes" aria-selected="false">
-                                <i class="fa fa-coins"></i>
-                                Transações
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" id="v-preferencias-tab" data-bs-toggle="pill"
-                               data-bs-target="#v-preferencias" type="button" role="tab"
-                               aria-controls="v-preferencias" aria-selected="false">
-                                <i class="fa-solid fa-arrow-right-arrow-left"></i>
-                                Preferências
-                            </a>
-                        </li>
-                    </ul>
-
-                    <div class="logout d-flex justify-content-center align-items-center">
-                        <i class="fa-sharp fa-solid fa-arrow-right-from-bracket"></i>
-                        <p>Sair</p>
-                    </div>
-                </div>
-            </nav>
-
-
-            <div class="tab-content justify-content-center">
                 <div class="tab-content" id="v-pills-tabContent">
 
                     <div class="fade show active" id="v-pills-perfil" role="tabpanel"
--- Site/ShelbyFc/resources/views/perfil/partials/transacoes.blade.php
+++ Site/ShelbyFc/resources/views/perfil/partials/transacoes.blade.php
@@ -1,7 +1,27 @@
 @if(empty($transacoes))
 
-    <h2>Voçê ainda não fez nenhuma transação</h2>
+    <h2>Você ainda não fez nenhuma transação</h2>
 
 @else
+
+
+    <table>
+        <tbody>
+        <tr class="header">
+            <th>#</th>
+            <th>Descrição</th>
+            <th>Valor</th>
+            <th>Data</th>
+        </tr>
+        @foreach($transacoes as $transacao)
+        <tr>
+            <td>{{$transacao->id}}</td>
+            <td>{{$transacao->descricao}}</td>
+            <td>{{$transacao->valor}}</td>
+            <td>{{$transacao->created_at}}</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
 
 @endif
