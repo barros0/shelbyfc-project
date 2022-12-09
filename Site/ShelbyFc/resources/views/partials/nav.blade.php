834b2c1ca85ecbfb1eb85bcc60354e44c0f88502
barros0 <joaoaobarros7@gmail.com>
2022-12-09 10:57:07 +0000

    update

--- Site/ShelbyFc/resources/views/partials/nav.blade.php
+++ Site/ShelbyFc/resources/views/partials/nav.blade.php
@@ -11,26 +11,34 @@
             <a href="{{route('register')}}">Register</a>
         </div>
     </div>
-    <div class="navbar-second-container d-flex justify-content-center">
-    <img src="{{asset('images/logo.svg')}}" width="7%" alt="Logo">
-    </div>
-    <div class="navbar-third-container">
-        <ul class="d-flex flex-row justify-content-center align-items-center">
-            <li>
-                <a href="">Loja Online</a>
-            </li>
-            <li>
-                <a href="{{route('forum.home')}}">Forúm</a>
-            </li>
-            <li>
-                <a href="{{route('noticias')}}">Notícias</a>
-            </li>
-            <li>
-                <a href="{{route('jogos')}}">Jogos</a>
-            </li>
-            <li>
-                <a href="{{route('contactos')}}">Suporte</a>
-            </li>
-        </ul>
-    </div>
+    <div class="navbar-second-container d-flex justify-content-around align-items-center">
+        <div class="patrocionadores d-flex align-items-center justify-content-center">
+            <img src="{{asset('images/icons/hp.svg')}}" width="10%" alt="nike_logo">
+            <img src="{{asset('images/icons/nike.svg')}}" width="10%" alt="nike_logo">
+        </div>
+        <img src="{{asset('images/logo.svg')}}" width="7%" alt="Logo">
+        <div class="patrocionadores d-flex align-items-center justify-content-center">
+            <img src="{{asset('images/icons/amazon.svg')}}" width="10%" alt="nike_logo">
+            <img src="{{asset('images/icons/nike.svg')}}" width="10%" alt="nike_logo">
+        </div>
+        </div>
+        <div class="navbar-third-container">
+            <ul class="d-flex flex-row justify-content-center align-items-center">
+                <li>
+                    <a href="">Loja Online</a>
+                </li>
+                <li>
+                    <a href="{{route('forum.home')}}">Forúm</a>
+                </li>
+                <li>
+                    <a href="{{route('noticias')}}">Notícias</a>
+                </li>
+                <li>
+                    <a href="{{route('jogos')}}">Jogos</a>
+                </li>
+                <li>
+                    <a href="{{route('contactos')}}">Suporte</a>
+                </li>
+            </ul>
+        </div>
 </nav>
