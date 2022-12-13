438a8e0bf2d1f60633a493488f1d48108c7512ef
barros0 <joaoaobarros7@gmail.com>
2022-12-13 16:27:56 +0000

    noticias seeder

--- Site/ShelbyFc/database/seeders/NoticiaTableSeeder.php
+++ Site/ShelbyFc/database/seeders/NoticiaTableSeeder.php
@@ -24,7 +24,13 @@ class NoticiaTableSeeder extends Seeder
                 'title' => 'ADEPTOS DO SHELBY FC RUMAM A FILADÉLFIA PARA A SEGUNDA LIGA EM DIRETO',
                 'small_description' => 'Centenas de adeptos da Cidade dos Eua e membros de Clubes Oficiais de Adeptos dirigiram-se a Filadélfia para o maior evento de sempre da Segunda Liga Live.',
                 'body' => 'Arranca a preparação para a receção ao SC Farense, da 3.ª jornada da Segunda Liga<br>Depois da vitória em Chaves (2-0), o FC Porto regressa ao trabalho na segunda-feira no Centro de Treinos e Formação Desportiva PortoGaia, no Olival.<br>A sessão (10h30) marca o arranque da preparação para o jogo da 3.ª jornada do grupo A da Taça da Liga, em que os Dragões recebem o Vizela na sexta-feira (20h30, Sport TV).',
-                'image' => 'nico-oreilly.webp',
+                'image' => 'noticia1.jpg',
+            ],
+            [
+                'title' => 'Shelby FC v CD Mafra ',
+                'small_description' => 'TICKETING INFORMATION',
+                'body' => 'O Clube recebeu uma dotação de 2.947 bilhetes, incluindo 18 baías de cadeira de rodas e uma série de bilhetes para adeptos ambulantes.',
+                'image' => 'noticia2.jpg',
             ],
         ];
 
