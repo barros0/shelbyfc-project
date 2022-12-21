c8a5e41a6bb1990313f1cd31cc6e99bdf1ef7549
barros0 <joaoaobarros7@gmail.com>
2022-12-21 21:06:51 +0000

    update

--- Site/ShelbyFc/resources/views/admin/news/index.blade.php
+++ Site/ShelbyFc/resources/views/admin/news/index.blade.php
@@ -1,29 +1,40 @@
 @extends('layouts.admin.master')
 
 
-@section('title','News | Shelby FC')
+@section('title', 'News | Shelby FC')
 
 @section('content')
 
 
     <h1>Noticias</h1>
-
-    <a href="{{route('admin.news.create')}}" class="btn">Nova</a>
-
+    <div class="form-menu d-flex justify-content-between">
+        <div class="form-search">
+            <input type="text" name="" class="form-control" placeholder="Search">
+        </div>
+        <a href="{{ route('admin.news.create') }}" class="btn">Adicionar</a>
+    </div>
     <table>
         <tbody>
-        <tr class="header">
-            <th>Pacote</th>
-            <th>Idade</th>
-            <th>Mensalidade</th>
-        </tr>
-        @foreach ($news as $new) <!--para cada registo de pacote-->
-        <tr>
-            <td>{{ $new->pack }}</td>
-            <td>{{ $new->age }}</td>
-            <td>{{ $new->price }}</td>
-        </tr>
-        @endforeach
+            <tr class="header">
+                <th>ID</th>
+                <th>Title</th>
+                <th>IMG</th>
+                <th>Views</th>
+                <th>Actions</th>
+            </tr>
+            @foreach ($news as $new)
+                <!--para cada registo de pacote-->
+                <tr>
+                    <td>{{ $new->id }}</td>
+                    <td>{{ $new->title }}</td>
+                    <td>{{ $new->image }}</td>
+                    <td>{{ $new->views }}</td>
+                    <td>
+                        <a href="{{ route('admin.news.create') }}" class="btn"><i class='bx bx-edit-alt' ></i></a>
+                        <a href="{{ route('admin.news.create') }}" class="btn"><i class='bx bx-trash' ></i></a>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 
