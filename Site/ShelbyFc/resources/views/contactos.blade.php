4731a3aee3ef130d5c3c566b9150a7c8132a0b27
João Duarte <joao.bartolomeu.tic@gmail.com>
2022-12-27 02:32:17 +0000

    up

--- Site/ShelbyFc/resources/views/contactos.blade.php
+++ Site/ShelbyFc/resources/views/contactos.blade.php
@@ -21,7 +21,33 @@
 
     </div>
 
+    <div class="what-section">
 
+    <div class="texto-banner"><span>|</span> DESCOBRE MAIS DO QUE FAZEMOS <span>|</span></div>
+
+    <div class="ret-section">
+
+    <div class="ret" id="ret1"></div>
+    <div class="ret" id="ret2"></div>
+    <div class="ret" id="ret3"></div>
+
+    </div>
+    </div>
+
+
+    <div class="team-section">
+    <div class="texto-banner"><span>|</span> A NOSSA EQUIPA <span>|</span></div>
+
+    <div class="ret-section">
+
+    <div class="ret" id="ret1"></div>
+    <div class="ret" id="ret2"></div>
+    <div class="ret" id="ret3"></div>
+    <div class="ret" id="ret4"></div>
+
+</div>
+
+    </div>
 
 
 </div>
