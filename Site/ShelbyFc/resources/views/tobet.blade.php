199107e51f8a00d68c0682774587407944c806a6
André Sousa <dev.andre.sousa@gmail.com>
2022-12-31 19:43:43 -0100

    updt

--- Site/ShelbyFc/resources/views/tobet.blade.php
+++ Site/ShelbyFc/resources/views/tobet.blade.php
@@ -12,7 +12,7 @@
             <div class="sidebar-apostas w-30">
 
                 <div class="row text-center">
-                    <h2>Próximos jogos</h2>
+                    <h2 class="text-center">Próximos jogos</h2>
                 </div>
 
             <div class="gametable_bet">
@@ -23,10 +23,28 @@
                             <img src="{{asset('images/logo.svg')}}" alt="">
                         </div>
 
+                        <div class="info-game d-flex justify-content-center flex-column align-content-between g-0 m-0 p-0">
+                            <p class="text-center fw-bolder">Amigável</p>
+                            <h4 class="vs text-center">VS</h4>
+                            <p class="text-center">15 Jan 2023</p>
+                        </div>
+
+                        <div class="team-img">
+                            <img src="{{asset('images/liga/sl_benfica.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="game-bet-option">
+                    <div class="d-flex flex-row justify-content-between">
+                        <div class="team-img">
+                            <img src="{{asset('images/liga/UD_Oliveirense.png')}}" alt="">
+                        </div>
+
                         <div class="info-game d-flex justify-content-center flex-column align-content-between">
                             <p class="text-center fw-bolder">Amigável</p>
                             <h4 class="vs text-center">VS</h4>
-                            <p class="text-center">15 jan 2023</p>
+                            <p class="text-center">15 Jan 2023</p>
                         </div>
 
                         <div class="team-img">
@@ -44,11 +62,11 @@
                         <div class="info-game d-flex justify-content-center flex-column align-content-between">
                             <p class="text-center fw-bolder">Amigável</p>
                             <h4 class="vs text-center">VS</h4>
-                            <p class="text-center">15 jan 2023</p>
+                            <p class="text-center">15 Jan 2023</p>
                         </div>
 
                         <div class="team-img">
-                            <img src="{{asset('images/logo.svg')}}" alt="">
+                            <img src="{{asset('images/liga/trofense.png')}}" alt="">
                         </div>
                     </div>
                 </div>
@@ -62,39 +80,21 @@
                         <div class="info-game d-flex justify-content-center flex-column align-content-between">
                             <p class="text-center fw-bolder">Amigável</p>
                             <h4 class="vs text-center">VS</h4>
-                            <p class="text-center">15 jan 2023</p>
+                            <p class="text-center">15 Jan 2023</p>
                         </div>
 
                         <div class="team-img">
-                            <img src="{{asset('images/logo.svg')}}" alt="">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="game-bet-option">
-                    <div class="d-flex flex-row justify-content-between">
-                        <div class="team-img">
-                            <img src="{{asset('images/logo.svg')}}" alt="">
-                        </div>
-
-                        <div class="info-game d-flex justify-content-center flex-column align-content-between">
-                            <p class="text-center fw-bolder">Amigável</p>
-                            <h4 class="vs text-center">VS</h4>
-                            <p class="text-center">15 jan 2023</p>
-                        </div>
-
-                        <div class="team-img">
-                            <img src="{{asset('images/logo.svg')}}" alt="">
+                            <img src="{{asset('images/liga/fc_penafiel.png')}}" alt="">
                         </div>
                     </div>
                 </div>
             </div>
             </div>
 
-            <div class="row text-center mx-0 w-70">
+            <div class="row text-center mx-0 g-0 p-0 w-70">
                 <h2>Selecione um jogo...</h2>
 
-                <div class="game-selected px-3 d-flex flex-column flex-wrap justify-content-center">
+                <div class="game-selected px-3 d-flex flex-column flex-wrap justify-content-evenly">
 
 
                     <div class="row g-0 m-0 p-0">
@@ -103,12 +103,21 @@
                     </div>
 
 
-                    <div class="row g-0 m-0 p-0"">
+                    <div class="row g-0 m-0 p-0">
 
-                        <input type="text" placeholder="Montante">
+                        <div class="montante">
+                        <span class="details-montante text-end"> <span class="cinza-montante">Min:</span> 1€ <span class="cinza-montante">Máx:</span> 500€</span>
+                        <input type="number" placeholder="Montante" min="1" max="500">
+                        <div class="montantes-rapidos">
+                            <button class="btn-secondary">1€</button>
+                            <button class="btn-secondary">10€</button>
+                            <button class="btn-secondary">25€</button>
+                            <button class="btn-secondary">50€</button>
+                        </div>
                     </div>
-                    <div class="row">
-                        <h3>Ganhos possiveis <span>200€</span></h3>
+                    </div>
+                    <div class="row g-0 m-0 p-0">
+                        <h3>Ganhos possiveis  <span class="ganhos-bg"><span class="ganhos">200€</span></span></h3>
                     </div>
 
 
