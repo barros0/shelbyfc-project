276e6f7b4c8ce2047cd654c6a04a1e93bfbb6faf
João Duarte <joao.bartolomeu.tic@gmail.com>
2023-01-10 16:23:47 +0000

    up contac

--- Site/ShelbyFc/resources/views/contactos.blade.php
+++ Site/ShelbyFc/resources/views/contactos.blade.php
@@ -4,6 +4,20 @@
 
 @section('content')
 
+<div class="conteudo-contactos">
+    <div class="imagem-inicio">
 
+      <div class="parte-baixo-banner">
+        <div class="texto-banner">CONTACTA-NOS</div>
+        <p>Encontre aqui mais informações de como nos pode contactar</p>
+      </div>
+    </div>
+
+    <div class="contact-section">
+            <div class="left-side"></div>
+            <div class="right-side"></div>
+    </div>
+
+</div>
 
 @endsection
