821302bbebd50091b8655c4903a4e2875e8c2c4c
João Duarte <joao.bartolomeu.tic@gmail.com>
2023-01-29 21:03:43 +0000

    up page terms

--- Site/ShelbyFc/resources/views/terms.blade.php
+++ Site/ShelbyFc/resources/views/terms.blade.php
@@ -1,10 +1,43 @@
 @extends('layouts.master')
 
-@section('title','Terms & Conditions | ShelbyFC')
+@section('title','Termos & Condições | ShelbyFC')
 
 @include('partials.head')
 
 @section('content')
 
+<div class="conteudo-terms">
+    <div class="imagem-inicio">
+
+        <div class="parte-baixo-banner">
+            <div class="texto-banner">Termos & Condições</div>
+            <p>Aqui pode encontrar os nossos termos e condições</p>
+        </div>
+    </div>
+
+    <div class="term-section">
+        <div class="term-box">
+            <div class="box-title">
+                <h2>Term & Conditions</h2>
+            </div>
+            <div class="box-content">
+                
+
+                @foreach ($terms as $term)
+                @if ($term->categoria == 'property')
+                     <h6>{!! $term->titulo !!}</h6>
+                @endif
+                    <p>{!! $term->texto !!}</p>
+            @endforeach
+
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
 
 @endsection
