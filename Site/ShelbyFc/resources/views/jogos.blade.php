12da739604e5c6aa8ad14e1e8be1dd6e3c420a0b
André Sousa <dev.andre.sousa@gmail.com>
2023-01-29 18:45:41 +0000

    Update jogos.blade.php

--- Site/ShelbyFc/resources/views/jogos.blade.php
+++ Site/ShelbyFc/resources/views/jogos.blade.php
@@ -50,6 +50,7 @@
                                 <p>Preço</p>
                                 <p>
                                     @if(Auth::check() || Auth::subscribed())
+                                            {{%}}
                                     @endif
                                 </p>
                             </div>
