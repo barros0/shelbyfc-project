8a67f05b0874b91ce720d375378b6043853a531a
João Duarte <joao.bartolomeu.tic@gmail.com>
2023-01-30 16:34:52 +0000

    up faqs fix bug

--- Site/ShelbyFc/public/js/functions.js
+++ Site/ShelbyFc/public/js/functions.js
@@ -1,5 +1,3 @@
-
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
--- Site/ShelbyFc/resources/views/faqs.blade.php
+++ Site/ShelbyFc/resources/views/faqs.blade.php
@@ -3,96 +3,112 @@
 @section('title', 'FaQS | Shelby FC')
 
 @section('content')
-    <div class="conteudo-faqs">
-        <div class="imagem-inicio">
+<div class="conteudo-faqs">
+    <div class="imagem-inicio">
 
-            <div class="parte-baixo-banner">
-                <div class="texto-banner">Apoio ao Cliente</div>
-                <p>Encontre aqui a resposta a todas as suas questões</p>
+        <div class="parte-baixo-banner">
+            <div class="texto-banner">Apoio ao Cliente</div>
+            <p>Encontre aqui a resposta a todas as suas questões</p>
+        </div>
+    </div>
+
+
+    <div class="section-faqs container">
+        <h1>Informações de sócio</h1>
+        @foreach ($faqs as $faq)
+        @if ($faq->categoria == 'informacoes')
+        <button class="accordion">{!! $faq->pergunta !!}</button>
+        @endif
+        <div class="panel">
+            <p>{!! $faq->resposta !!}</p>
+            <br>
+        </div>
+        @endforeach
+
+
+
+
+
+        <h1>Criar conta</h1>
+        @foreach ($faqs as $faq)
+        @if ($faq->categoria == 'criar_conta')
+        <button class="accordion">{!! $faq->pergunta !!}</button>
+        @endif
+        <div class="panel">
+            <p>{!! $faq->resposta !!}</p>
+            <br>
+        </div>
+        @endforeach
+
+
+
+
+        <h1>Jogos e Apostas</h1>
+        @foreach ($faqs as $faq)
+        @if ($faq->categoria == 'jogos_apostas')
+        <button class="accordion">{!! $faq->pergunta !!}</button>
+        @endif
+        <div class="panel">
+            <p>{!! $faq->resposta !!}</p>
+            <br>
+        </div>
+        @endforeach
+
+
+
+
+
+        <h1>Depósitos e Levantamentos</h1>
+        @foreach ($faqs as $faq)
+        @if ($faq->categoria == 'depositos_levantamentos')
+        <button class="accordion">{!! $faq->pergunta !!}</button>
+        @endif
+        <div class="panel">
+            <p>{!! $faq->resposta !!}</p>
+            <br>
+        </div>
+        @endforeach
+
+
+
+    </div>
+
+    <div class="fale-conosco">
+        <div class="imagem-fale-conosco">
+            <div class="lado-esquerdo">
+                <div class="titulo-fale-conosco">
+                    <p> Fale Connosco</p>
+                </div>
+                <div class="texto-fale-conosco">
+                    <p>Para entrar em contacto connosco clique, por favor,</p>
+                    <p> no botão para enviar a sua mensagem.</p>
+                </div>
+                <div class="btn-section">
+                    <button type="button" onclick="location.href='{{ route('contacts') }}'" class="btn-send">Enviar
+                        Mensagem</button>
+                </div>
             </div>
-        </div>
-
-
-        <div class="section-faqs container">
-            <h1>Informações de sócio</h1>
-            @foreach ($faqs as $faq)
-                @if ($faq->categoria == 'informacoes')
-                    <button class="accordion">{!! $faq->pergunta !!}</button>
-                @endif
-                <div class="panel">
-                    <p>{!! $faq->resposta !!}</p>
-                    <br>
-                </div>
-            @endforeach
-
-
-
-
-
-            <h1>Criar conta</h1>
-            @foreach ($faqs as $faq)
-                @if ($faq->categoria == 'criar_conta')
-                    <button class="accordion">{!! $faq->pergunta !!}</button>
-                @endif
-                <div class="panel">
-                    <p>{!! $faq->resposta !!}</p>
-                    <br>
-                </div>
-            @endforeach
-
-
-
-
-            <h1>Jogos e Apostas</h1>
-            @foreach ($faqs as $faq)
-                @if ($faq->categoria == 'jogos_apostas')
-                    <button class="accordion">{!! $faq->pergunta !!}</button>
-                @endif
-                <div class="panel">
-                    <p>{!! $faq->resposta !!}</p>
-                    <br>
-                </div>
-            @endforeach
-
-
-
-
-
-            <h1>Depósitos e Levantamentos</h1>
-            @foreach ($faqs as $faq)
-                @if ($faq->categoria == 'depositos_levantamentos')
-                    <button class="accordion">{!! $faq->pergunta !!}</button>
-                @endif
-                <div class="panel">
-                    <p>{!! $faq->resposta !!}</p>
-                    <br>
-                </div>
-            @endforeach
-
-
+            <div class="lado-direito"></div>
 
         </div>
 
-        <div class="fale-conosco">
-            <div class="imagem-fale-conosco">
-                <div class="lado-esquerdo">
-                    <div class="titulo-fale-conosco">
-                        <p> Fale Connosco</p>
-                    </div>
-                    <div class="texto-fale-conosco">
-                        <p>Para entrar em contacto connosco clique, por favor,</p>
-                        <p> no botão para enviar a sua mensagem.</p>
-                    </div>
-                    <div class="btn-section">
-                        <button type="button" class="btn-send">Enviar Mensagem</button>
-                    </div>
-                </div>
-                <div class="lado-direito"></div>
+    </div>
+    <script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-            </div>
-
-        </div>
-
-
-
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                panel.classList.remove("active2");
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                panel.classList.add("active2");
+            }
+        });
+    }
+    </script>
     @endsection
