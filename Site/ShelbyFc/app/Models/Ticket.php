ec7a5b96b88a0f358165d142f8b5a2c8f786e3b2
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2023-01-30 17:16:27 +0000

    TicketsController

--- Site/ShelbyFc/app/Models/Ticket.php
+++ Site/ShelbyFc/app/Models/Ticket.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Ticket extends Model
 {
     use HasFactory;
-    use SoftDeletes;
     protected $fillable = [
         'user_id',
         'game_id',
