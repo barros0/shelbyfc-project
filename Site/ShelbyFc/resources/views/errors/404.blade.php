da842c6c56b477dd55791a37ab9ac531d251f262
João Duarte <joao.bartolomeu.tic@gmail.com>
2023-01-30 15:47:54 +0000

    404 page

--- Site/ShelbyFc/resources/views/errors/404.blade.php
+++ Site/ShelbyFc/resources/views/errors/404.blade.php
@@ -3,4 +3,16 @@
 @section('title', 'Error 404 | Shelby FC')
 
 @section('content')
+<div class="conteudo-pagina">
+    <div class="caixa-erro">
+        <div class="left-side">
+            <p>404</p>
+        </div>
+        <div class="right">
+            <h6>DESCULPE!</h6><br>
+            <p>A página que está à procura não foi encontrada. </p>
+        </div>
+    </div>
+    <button onclick="location.href='{{ route('index') }}'" type="button">Voltar para o Estádio</button>
+</div>
 @endsection
