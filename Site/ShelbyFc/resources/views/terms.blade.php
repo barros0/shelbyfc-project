ace892a99407cb0a25d188afecc8551e0f1cff25
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2023-01-30 17:19:20 +0000

    bruh

--- Site/ShelbyFc/resources/views/terms.blade.php
+++ Site/ShelbyFc/resources/views/terms.blade.php
@@ -22,79 +22,14 @@
                 <h2>Term & Conditions</h2>
             </div>
             <div class="box-content">
-                
+
 
                 @foreach ($terms as $term)
-                @if ($term->categoria == 'property')
                      <h6>{!! $term->titulo !!}</h6>
                     <p>{!! $term->texto !!}</p>
-                    @endif
             @endforeach
 
 
-              @foreach ($terms as $term)
-                @if ($term->categoria == 'privacy')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'intellectual')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-           
-
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'responsability')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'registration')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'user')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'sale')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'member')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'ticket')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'purchase_sale')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
-            @foreach ($terms as $term)
-                @if ($term->categoria == 'final_consideration')
-                     <h6>{!! $term->titulo !!}</h6>             
-                      <p>{!! $term->texto !!}</p>
-                    @endif
-            @endforeach
             </div>
         </div>
     </div>
