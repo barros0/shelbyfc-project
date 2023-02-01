51f877945b148bd3800fff8be98b80fef8bbd1c9
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2023-02-01 13:41:38 +0000

    contacts +-

--- Site/ShelbyFc/resources/views/contacts.blade.php
+++ Site/ShelbyFc/resources/views/contacts.blade.php
@@ -16,7 +16,7 @@
 
     <div class="contact-section container">
         <div class="row">
-        <div class="left-side col-md-6">
+        <div class="left-side col-lg-6">
             <div class="up-side">
                 <h2>Vamos entrar em contacto?</h2>
                 <p>Estamos disponiveis para o ajudar da melhor maneira.</p>
@@ -39,32 +39,32 @@
             </div>
         </div>
         </div>
-        <div class="right-side col-md-6">
+        <div class="right-side col-12 col-lg-6">
             <form action="{{ route('contacts.create') }}" method="post" enctype="multipart/form-data" class="form-contact">
               @method('post')
                 @csrf
-                <div class="top-form">
-                    <div class="input-icons">
-                        <i class='bx bx-user'></i>
-                        <input class="input-field" type="text" id="nome" name="name" placeholder="Nome" required value="{{ Auth::check() ? Auth::user()->name : '' }}">
-                    </div>
-                    <div class="input-icons">
-                        <i class='bx bx-phone'></i>
-                        <input class="input-field" type="text" id="telefone" name="phone" placeholder="Telefone" value="{{ Auth::check() ? Auth::user()->phone : '' }}"
-                            required>
-                    </div>
+<div class="row">
+                <div class="input-icons col-md-12 col-lg-6">
+                    <i class='bx bx-user'></i>
+                    <input class="input-field" type="text" id="nome" name="name" placeholder="Nome" required value="{{ Auth::check() ? Auth::user()->name : '' }}">
                 </div>
-                <div class="input-icons">
+                <div class="input-icons  col-md-12 col-lg-6">
+                    <i class='bx bx-phone'></i>
+                    <input class="input-field" type="text" id="telefone" name="phone" placeholder="Telefone" value="{{ Auth::check() ? Auth::user()->phone : '' }}"
+                           required>
+                </div>
+                <div class="input-icons col-md-12">
                     <i class='bx bx-envelope'></i>
                     <input class="input-field" type="text" id="email" name="email" placeholder="E-mail" value="{{ Auth::check() ? Auth::user()->email : '' }}" required >
                 </div>
-                <div class="input-icons">
+                <div class="input-icons col-md-12">
                     <i class='bx bx-captions'></i>
                     <input class="input-field" type="text" id="assunto" name="subject" placeholder="Assunto"  required>
                 </div>
                 <textarea id="mensagem" name="message" rows="4" cols="50" placeholder="Em que podemos ajudar?"
                     required></textarea>
                 <input type="submit" class="btn" value="Enviar Mensagem">
+        </div>
             </form>
         </div>
         </div>
