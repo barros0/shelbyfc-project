985fd0d1b1c1d20157e034bc76ba59da7abb76ab
MicaG28 <65852191+MicaG28@users.noreply.github.com>
2023-02-01 15:22:48 +0000

    nav admin active

--- Site/ShelbyFc/app/Http/Controllers/AdminController.php
+++ Site/ShelbyFc/app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
         $news = News::count();
         $users = User::count();
         $contact = Contact::count();
-        $subscription = Subscription::count();
+        $subscription = Subscription::Active()->count();
         $forum_posts = Forum_post::orderBy('created_at', 'desc')->take(1)->get();
 
         $lastgames = Game::orderBy('created_at', 'desc')->LastGames()->get()->take(2);
@@ -27,12 +27,4 @@ class AdminController extends Controller
         return view('admin.index', compact('news', 'users', 'subscription', 'contact', 'forum_posts', 'lastgames','nextgames'));
     }
 
-
-    public function contacts()
-    {
-
-        $contacts = Contact::orderby('created_at', 'desc')->get();
-
-        return view('admin.contacts.index', compact('contacts'));
-    }
 }
