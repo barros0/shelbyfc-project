3ba16e30496375e52c14de803023213a667ee327
barros0 <joaoaobarros7@gmail.com>
2023-02-01 04:15:10 +0000

    update forum css full

--- Site/ShelbyFc/resources/views/forum/post.blade.php
+++ Site/ShelbyFc/resources/views/forum/post.blade.php
@@ -5,14 +5,14 @@
 @section('content')
     <div class="content_post">
         <div>
-            <p>{{ $post->user->name }},
+            <p class="post_date">{{ $post->user->name }},
                 <span style="color: rgb(0 0 0 / 60%);">{{ $post->created_at }}</span>
             </p>
             <div class="user_info_forum">
                 <img class="post_user_img" src="{{ asset('images/users/' . $post->user->image) }}" alt="user">
                 <div class="content">
                     <p style="font-size: 20px; font-weight:bolder;">{{ $post->title }}</p>
-                    <p style="font-size: 20px">{{ $post->body }}</p>
+                    <p class="post_body">{{ $post->body }}</p>
                     @foreach ($posts_images as $image)
                         @if ($image->post_id === $post->id)
                             <img class="post_img" src="{{ asset('images/forum_posts_images/' . $image->image) }}"
@@ -42,7 +42,9 @@
                     <img src="{{ asset('images/users/' . $comment->user->image) }}" alt="img_user">
                     <div class="container_post_info d-flex flex-column">
                         <p style="font-weight: 800;">{{ $comment->user->name }}</p>
-                        <p>{{ $comment->comment }}</p>
+                        <div class="post_comment">
+                            {{ $comment->comment }}
+                        </div>
                     </div>
                     @foreach ($comment->replies as $reply)
                         {{ $reply->comment }}
